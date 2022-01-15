57882de260559250e5c1bd8448d22006943fce3d
xtinalucia <christina.lucia123@gmail.com>
2022-01-14 21:09:57 -0600

    2 routines 6 tests  added

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -24,6 +24,103 @@ def test_clear_board():
     # of my list
     assert lst[0] == " "
     assert lst == list(" "*9)
+    
+def test_display_board_empty():
+    ib = tictactoe.init_board()
+    v = tictactoe.display_board(ib)
+    
+    res = "   |   |   \n"+\
+          "===========\n"+\
+          "   |   |   \n"+\
+          "===========\n"+\
+          "   |   |   \n"
+    assert len(res.split("\n")) == len(v.split("\n"))
+    
+    for rline, vline in zip(res.split("\n"), v.split("\n")):
+        print(f"\n{rline=}\n{vline=}")
+        assert vline == rline
+        
+def test_display_board_X():
+    ib = tictactoe.init_board()
 
+    for idx in range(9):
+        ib[idx] = 'X'
+
+    v = tictactoe.display_board(ib)
+
+    res = " X | X | X\n" + \
+          "===========\n" + \
+          " X | X | X\n" + \
+          "===========\n" + \
+          " X | X | X\n"
+          
+    assert len(res.split("\n")) == len(v.split("\n"))
+
+    for rline, vline in zip(res.split("\n"), v.split("\n")):
+        print(f"\n{rline=}\n{vline=}")
+        assert vline == rline
+
+def test_display_board_O():
+    ib = tictactoe.init_board()
+
+    for idx in range(9):
+        ib[idx] = 'O'
+
+    v = tictactoe.display_board(ib)
+
+    res = " O | O | O\n" + \
+          "===========\n" + \
+          " O | O | O\n" + \
+          "===========\n" + \
+          " O | O | O\n"
+
+    assert len(res.split("\n")) == len(v.split("\n"))
+
+    for rline, vline in zip(res.split("\n"), v.split("\n")):
+        print(f"\n{rline=}\n{vline=}")
+        assert vline == rline
+def test_display_board_all_rows_XO():
+    for ch in ["X","O"]:
+        # Row 1
+        b = tictactoe.init_board()
+        b[0] = b[1] = b[2] = ch
+        assert tictactoe.winner(ch, b) == True 
+
+        # Row 2
+        b = tictactoe.init_board()
+        b[3] = b[4] = b[5] = ch
+        assert tictactoe.winner(ch, b) == True 
+
+        # Row 3
+        b = tictactoe.init_board()
+        b[6] = b[7] = b[8] = ch
+        assert tictactoe.winner(ch, b) == True
     
+def test_display_board_all_cols_XO():
+    for ch in ["X","O"]:
+        # cols 1
+        b = tictactoe.init_board()
+        b[0] = b[3] = b[6] = ch
+        assert tictactoe.winner(ch, b) == True 
+
+        # cols 2
+        b = tictactoe.init_board()
+        b[1] = b[4] = b[7] = ch
+        assert tictactoe.winner(ch, b) == True 
+
+        # cols 3
+        b = tictactoe.init_board()
+        b[2] = b[5] = b[8] = ch
+        assert tictactoe.winner(ch, b) == True
     
+def test_display_board_all_diag_XO():
+    for ch in ["X","O"]:
+        # cols 1
+        b = tictactoe.init_board()
+        b[0] = b[4] = b[8] = ch
+        assert tictactoe.winner(ch, b) == True 
+
+        # cols 2
+        b = tictactoe.init_board()
+        b[2] = b[4] = b[6] = ch
+        assert tictactoe.winner(ch, b) == True
--- tictactoe.py
+++ tictactoe.py
@@ -33,27 +33,56 @@ def clear_board( mylist ):
 
     return mylist
 
+def get_names():
+    first = input("Enter first Player's name: ")
+    second = input("Enter second Player's name: ")
+    return (first, second)
+
+def display_board(b):
+    return f" {b[0]} | {b[1]} | {b[2]}\n" + \
+            "===========\n" + \
+           f" {b[3]} | {b[4]} | {b[5]}\n" + \
+            "===========\n" + \
+           f" {b[6]} | {b[7]} | {b[8]}\n"
+
+def winner(tc, board):
+    tc = get_names
+    # need to write code for testing Rows
+    # need to write code for testing Columns
+    # need to write code for testing Diag
+
+
+    return False 
+
 def main():
-    
+
     # init board 
     board = init_board()
     
     # ask users names 
-    # (first, second) = get_names()
+    (first, second) = get_names()
 
     # for up 9 moves, switch users, X is first, 
+    for moves in range(9):
 
         # display board( board )
+        db = display_board(board)
+        print(db)
 
-        # ask user 1,2 to place piece 
-
-            # check if piece can go there, else loop to ask again 
-
+        # ask user 1,2 to place piece (0,..,8)
+        # pos = ask_user_position(board)
+        #        -- routine, is_pos_taken(pos, board)
 
         # CHECK IF WINNER??
             # if yes, break, declare the user 
-
+        if winner(tc, board):
+            # print message, like Bob is the Winner!
+            break 
     # if no winner, declare draw 
-    
-if __name__ != "__main__":
+
+    # Print nobody has won, it's a draw!
+
+
+
+if __name__ == "__main__":
     main()
